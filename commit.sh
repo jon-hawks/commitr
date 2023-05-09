ba05c48a63ca6d8f0fb9e2f9b673f415bd3fa980
Jon Hawks <17555162+jon-hawks@users.noreply.github.com>
2023-05-09 03:22:04 -0400

    Fix token in URL for commit script.

--- commit.sh
+++ commit.sh
@@ -4,14 +4,14 @@
 ################################################################################
 
 # Wait somewhere between 1 second and 1 day.
-# sleep $(( ($(awk 'BEGIN { srand(); print int(rand()*32768) }' /dev/null) % 1440) * 60 + ($(awk 'BEGIN { srand(); print int(rand()*32768) }' /dev/null) % 60) ))
+# sleep $(( ($(awk 'BEGIN { srand(); print int(rand() * 32768) }' /dev/null) % 1440) * 60 + ($(awk 'BEGIN { srand(); print int(rand() * 32768) }' /dev/null) % 60) ))
 sleep 30
 
 # Install Git.
 apk add --no-cache git
 
 # Clone this repository.
-git clone --depth 1 --no-tags --single-branch "https://$GITHUB_TOKEN@github.com/$GITHUB_REPO.git"
+git clone --depth 1 --no-tags --single-branch "https://$GITHUB_TOKEN:@github.com/$GITHUB_REPO.git"
 cd commitr || exit 1
 
 # Commit a file.
