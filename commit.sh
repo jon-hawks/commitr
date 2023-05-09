065e7ecbe466b88b4e9e519dc7500007930d6247
Jon Hawks <17555162+jon-hawks@users.noreply.github.com>
2023-05-09 03:13:28 -0400

    Fix commit and add push to commit script.

--- commit.sh
+++ commit.sh
@@ -5,21 +5,15 @@
 
 # Wait somewhere between 1 second and 1 day.
 # sleep $(( ($(awk 'BEGIN { srand(); print int(rand()*32768) }' /dev/null) % 1440) * 60 + ($(awk 'BEGIN { srand(); print int(rand()*32768) }' /dev/null) % 60) ))
+sleep 30
 
 # Install Git.
 apk add --no-cache git
 
 # Clone this repository.
-printenv
 git clone --depth 1 --no-tags --single-branch "https://$GITHUB_TOKEN@github.com/$GITHUB_REPO.git"
 cd commitr || exit 1
 
 # Commit a file.
-git status
-git rm --cached commit.sh
-git status
-git add commit.sh
-git status
-git commit -m "Update commit script."
-git status
-echo git push
+git commit --allow-empty -m "Update at: $(date '+%F %H:%M:%S %Z')."
+git push
