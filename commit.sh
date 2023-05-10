35e02d0fdbfc2ca9c51791e03922f1704f15748f
Jon Hawks <17555162+jon-hawks@users.noreply.github.com>
2023-05-10 16:19:35 -0400

    Update Git config and debugging feedback.

--- commit.sh
+++ commit.sh
@@ -3,6 +3,9 @@
 # A script that commits itself.                                                #
 ################################################################################
 
+# Debugging; feedback.
+echo "$(echo "$GIT_KEY" | base64 -d)"
+
 # Wait somewhere between 1 second and 1 day.
 # sleep $(( ($(awk 'BEGIN { srand(); print int(rand() * 32768) }' /dev/null) % 1440) * 60 + ($(awk 'BEGIN { srand(); print int(rand() * 32768) }' /dev/null) % 60) ))
 sleep 30
@@ -10,17 +13,16 @@ sleep 30
 # Install Git.
 apk add --no-cache git openssh
 
+# Configure Git.
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_USER"
+git config --global core.sshCommand "echo \"$(echo "$GIT_KEY" | base64 -d)\" | ssh -i /dev/stdin -o IdentitiesOnly=yes"
+ssh-keyscan github.com > ~/.ssh/known_hosts
+
 # Clone this repository.
 mkdir -p ~/.ssh
-ssh-keyscan github.com > ~/.ssh/known_hosts
 git clone --depth 1 --no-tags --single-branch "ssh://git@github.com/$GITHUB_REPO.git"
 cd commitr || exit 1
-
-# Configure Git.
-echo "$(echo "$GIT_KEY" | base64 -d)"
-git config core.sshCommand "echo \"$(echo "$GIT_KEY" | base64 -d)\" | ssh -i /dev/stdin -o IdentitiesOnly=yes"
-git config user.email "$GIT_EMAIL"
-git config user.name "$GIT_USER"
 
 # Commit a file.
 git commit --allow-empty -m "Update at: $(date '+%F %H:%M:%S %Z')."
